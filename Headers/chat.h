262c942f01de48081d2825e11dced149a874f7c9
George562 <mnk_02@mail.ru>
2023-07-15 12:34:48 +0300

    code refactor

--- Headers/chat.h
+++ Headers/chat.h
@@ -1,5 +1,7 @@
 #include "text.h"
 
+#define SPACE_BETWEEN_LINES_IN_PIXELS 35
+
 ////////////////////////////////////////////////////////////
 // Class
 ////////////////////////////////////////////////////////////
@@ -9,15 +11,16 @@ public:
     str buffer;
     bool inputted;
     sf::Time times[11];
-    int start, len, CharacterSize;
+    std::vector<str> lines;
+    int start, len;
     size_t cursorPos;
     sf::RectangleShape rect, cursor;
-    float dy;
     std::map<str, void (*)(void)> commands;
+    bool ChatEnable;
 
     Chat();
     void draw(sf::RenderWindow&);
-    void InputText(sf::Event&);
+    bool InputText(sf::Event&);
     bool Entered();
     void addLine(str);
     str Last();
@@ -29,41 +32,47 @@ public:
 // Realization
 ////////////////////////////////////////////////////////////
 
-Chat::Chat() : PlacedText(), inputted(false), start(0), len(11), CharacterSize(27) {
-    dy = 35;
-    PosX = 150; PosY = sch - dy * (len + 2);
+Chat::Chat() : PlacedText(), inputted(false), start(0), len(11) {
+    PosX = 150; PosY = sch - SPACE_BETWEEN_LINES_IN_PIXELS * (len + 2);
     Width = scw - PosX * 2; Height = 35;
     lines.assign(11, "");
-    text.setCharacterSize(CharacterSize);
+
+    text.setCharacterSize(27);
     text.setFillColor(sf::Color(199, 199, 199, 180));
     text.setString("");
+
     rect.setFillColor(sf::Color(50, 50, 50, 100));
     rect.setOutlineColor(sf::Color::White);
     rect.setOutlineThickness(2);
-    rect.setPosition(PosX, PosY + dy * len);
+    rect.setPosition(PosX, PosY + SPACE_BETWEEN_LINES_IN_PIXELS * len);
     rect.setSize({Width, Height});
+
     cursor.setFillColor(sf::Color::White);
     cursor.setSize({3, Height});
+
+    ChatEnable = true;
 }
 
 void Chat::draw(sf::RenderWindow& window) {
+    if (!ChatEnable) return;
     if (inputted) {
         window.draw(rect);
         PlacedText tempText; tempText.text.setString(lines[start].substr(0, cursorPos));
-        tempText.text.setCharacterSize(CharacterSize);
-        cursor.setPosition(PosX + 5 + tempText.text.getLocalBounds().width, PosY + dy * len);
+        tempText.text.setCharacterSize(text.getCharacterSize());
+        cursor.setPosition(PosX + 5 + tempText.text.getLocalBounds().width, PosY + SPACE_BETWEEN_LINES_IN_PIXELS * len);
         if (GlobalClock.getElapsedTime() % sf::seconds(1.f) > sf::seconds(1.f / 2))
             window.draw(cursor);
     }
     for (int i = 0; i < len; i++)
         if (GlobalClock.getElapsedTime() - times[(start - i + len) % len] < sf::seconds(5) || inputted) {
             text.setString(lines[(start - i + len) % len]);
-            text.setPosition(PosX + 5, PosY + dy * (len - i) + 4);
+            text.setPosition(PosX + 5, PosY + SPACE_BETWEEN_LINES_IN_PIXELS * (len - i) + 4);
             window.draw(text);
         }
 }
 
-void Chat::InputText(sf::Event& event) {
+bool Chat::InputText(sf::Event& event) {
+    if (!ChatEnable) return false;
     setlocale(LC_ALL, "rus");
     buffer.clear();
     if (inputted && event.type == sf::Event::TextEntered && 32 <= event.text.unicode) {
@@ -73,15 +82,21 @@ void Chat::InputText(sf::Event& event) {
         cursorPos++;
     }
     if (event.type == sf::Event::KeyPressed) {
-        if (event.key.code == sf::Keyboard::BackSpace && lines[start].size() > 0 && cursorPos > 0)
+        if (event.key.code == sf::Keyboard::Escape)
+            inputted = false;
+        else if (event.key.code == sf::Keyboard::Enter)
+            Entered();
+
+        if (event.key.code == sf::Keyboard::BackSpace && cursorPos > 0)
             lines[start].erase(lines[start].begin() + cursorPos-- - 1);
-        else if (event.key.code == sf::Keyboard::Left && cursorPos > 0) cursorPos--;
-        else if (event.key.code == sf::Keyboard::Right && cursorPos < lines[start].length()) cursorPos++;
-        else if (event.key.code == sf::Keyboard::Home) cursorPos = 0;
-        else if (event.key.code == sf::Keyboard::End) cursorPos = lines[start].size();
         else if (event.key.code == sf::Keyboard::Delete && cursorPos < lines[start].size())
             lines[start].erase(lines[start].begin() + cursorPos);
+        else if (event.key.code == sf::Keyboard::Left && cursorPos > 0) cursorPos--;
+        else if (event.key.code == sf::Keyboard::Right && cursorPos < lines[start].size()) cursorPos++;
+        else if (event.key.code == sf::Keyboard::Home) cursorPos = 0;
+        else if (event.key.code == sf::Keyboard::End) cursorPos = lines[start].size();
     }
+    return inputted || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Enter);
 }
 
 bool Chat::Entered() {
