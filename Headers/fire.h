63a93af3357bb7132e3cee971d9adf006441f16f
vorodan <nnadorovivan@gmail.com>
2024-02-02 19:41:56 +0300

    Теперь огонь точно не выходит за стены и пределы уровня

--- Headers/fire.h
+++ Headers/fire.h
@@ -25,26 +25,30 @@ public:
     void Propagation() {
         float dist = 3 * Width / 2;
         float angle;
+        int y, x;
         descendant1 = new Fire(sf::seconds(rand() % 4 + 1.f), loc);
         descendant1->setSize(Width, Height);
         do {
             angle = (rand() % 360) * M_PI / 180;
             descendant1->setPosition(PosX + dist * std::sin(angle), PosY + dist * std::cos(angle));
             descendant1->setAnimation(Textures::Fire, 1, 1, sf::seconds(1), &Shaders::Map);
-        } while (!loc->EnableTiles[(int)descendant1->PosY / size][(int)descendant1->PosX / size] ||
-                    !PropagationAllowed((int)descendant1->PosY / size, (int)descendant1->PosX / size));
+            y = (int)descendant1->PosY / size; x = (int)descendant1->PosX / size;
+        } while (!loc->EnableTiles[y][x] || !PropagationAllowed(descendant1->PosY, descendant1->PosX));
+
         descendant2 = new Fire(sf::seconds(rand() % 4 + 1.f), loc);
         descendant2->setSize(Width, Height);
         do {
             angle = (rand() % 360) * M_PI / 180;
             descendant2->setPosition(PosX + dist * std::sin(angle), PosY + dist * std::cos(angle));
             descendant2->setAnimation(Textures::Fire, 1, 1, sf::seconds(1), &Shaders::Map);
-        } while (!loc->EnableTiles[(int)descendant2->PosY / size][(int)descendant2->PosX / size] ||
-                    !PropagationAllowed((int)descendant2->PosY / size, (int)descendant2->PosX / size));
+            y = (int)descendant2->PosY / size; x = (int)descendant2->PosX / size;
+        } while (!loc->EnableTiles[y][x] || !PropagationAllowed(descendant2->PosY, descendant2->PosX));
     }
 
-    bool PropagationAllowed(int y, int x) {
+    bool PropagationAllowed(float pos_y, float pos_x) {
         int y_ancestor = (int)PosY / size; int x_ancestor = (int)PosX / size;
+        if (pos_y < 0 || pos_x < 0) return false; // если огонь пытается пересечь самые левые или самые верхние стены
+        int y = (int)pos_y / size; int x = (int)pos_x / size;
         if (y_ancestor == y && x_ancestor == x) return true; // одинаковые тайлы
         // проверка на стену между тайлами
         if (y_ancestor == y && !loc->walls[y * 2 + 1][std::max(x_ancestor, x)] || // соседние тайлы по горизонтали
