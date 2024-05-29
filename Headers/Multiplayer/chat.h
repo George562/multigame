05f8dd229f252ce8360e1159318565d5b79bb4eb
George562 <mnk_02@mail.ru>
2024-05-30 00:52:53 +0300

    fixed warning

--- Headers/Multiplayer/chat.h
+++ Headers/Multiplayer/chat.h
@@ -105,8 +105,10 @@ bool Chat::InputText(sf::Event& event) {
         if (event.key.code == sf::Keyboard::Enter)
             Entered();
 
-        if (event.key.code == sf::Keyboard::BackSpace && cursorPos > 0)
-            lines[start].setString(lines[start].getString().substring(0, cursorPos) + lines[start].getString().substring(cursorPos--, lines[start].TextSize()));
+        if (event.key.code == sf::Keyboard::BackSpace && cursorPos > 0) {
+            lines[start].setString(lines[start].getString().substring(0, cursorPos) + lines[start].getString().substring(cursorPos, lines[start].TextSize()));
+            cursorPos--;
+        }
         if (event.key.code == sf::Keyboard::Delete && cursorPos < lines[start].TextSize())
             lines[start].setString(lines[start].getString().substring(0, cursorPos) + lines[start].getString().substring(cursorPos + 1, lines[start].TextSize()));
         if (event.key.code == sf::Keyboard::Left && cursorPos > 0) cursorPos--;
